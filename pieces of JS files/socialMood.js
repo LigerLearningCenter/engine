11bdf6ffe237eee160837fb8767a9dbf7e259ab0
KetyaNop <ketya.n@ligercambodia.org>
2016-05-04 19:46:11 +0700

    reorganized the folder and broke code into smaller pieces

---
+++ pieces of JS files/socialMood.js
@@ -0,0 +1,11 @@
+//randomEvents of socialMood
+var socialMood = {
+    industry: 100,
+    agriculture: 100,
+    consumerServices: 100,
+    infrastructure: 100,
+    education: 100,
+    tourism: 100,
+    ecology: 100
+};
+var randomEvents = []; //the data team will have this **we can access it with the code ||EX. RI01.discription or RI01.industry.effect
