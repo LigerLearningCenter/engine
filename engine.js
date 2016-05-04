648c82698a69eb5d125bab08d44ef07abeec123c
KetyaNop <ketya.n@ligercambodia.org>
2016-05-04 10:13:10 +0700

    Added coefficient, mileStoneLevel, socialMood and some other function

--- engine.js
+++ engine.js
@@ -1,5 +1,42 @@
-var sectors ={};//the 7 sectors is created inside object *sectors* as object
-var initBudget = 2000;//start up budget
 function mainLoop(socialMood, sectors, budget, mileStone){
 	//This loop will run every 4.2 seconds
+	//It will set new budget, reset the money in different sectors
+	//Calculate
 }
+
+function increaseMileStoneLevel (name){
+	mileStoneLevel[name] += 1; 
+}
+
+function mileStoneEffect(sectors){
+
+}
+
+//***************************************************************************************************//
+var socialMood = {
+	industry:100,
+	agriculture:100,
+	consumerServices:100,
+	infrastructure:100,
+	education:100,
+	tourism:100,
+	ecology:100
+};
+var mileStoneLevel = {
+	industry:0,
+	agriculture:0,
+	consumerServices:0,
+	infrastructure:0,
+	education:0,
+	tourism:0,
+	ecology:0
+};
+var coefficient = {
+	industry:1.10,
+	agriculture:1.10,
+	consumerServices:1.10,
+	infrastructure:1.10,
+	education:1.10,
+	tourism:1.10,
+	ecology:1.10
+};
