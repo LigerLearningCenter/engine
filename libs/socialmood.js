544233d9ff49d214836e84f8bf315f1f391651cb
neangloeurng <sreyneang.l@ligercambodia.org>
2016-05-10 15:44:45 +0700

    add a function to choose random number
    The function will take 2 parameter "min, max"
    the function will return the number between the 2 parameters "min" is included but "max" is excluded

--- libs/socialmood.js
+++ libs/socialmood.js
@@ -1,4 +1,9 @@
 //randomEvents of socialMood
+//we can use this method to choose randomly what event will happen
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 var socialMood = {
     industry: 100,
     agriculture: 100,
@@ -8,4 +13,4 @@ var socialMood = {
     tourism: 100,
     ecology: 100
 };
-var randomEvents = []; //the data team will have this **we can access it with the code ||EX. RI01.discription or RI01.industry.effect
+var randomEvents = []; //the data team will have this **we can access it with the code ||EX. randomEvents[0].RI01.discription
