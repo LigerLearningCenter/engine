908da6c6b32d08ff137aa979ecbc2155f296f0eb
djflkjs <sreyneang.l@ligercambodia.org>
2016-05-16 14:30:38 +0700

    Added new code for random event
    create a new function for choosing random event

--- libs/random_event.js
+++ libs/random_event.js
@@ -2,28 +2,8 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-
-function getRandomEvent(type){
-  if (type === "all"){
-      return allEvents[getRandomNumber[0, allEvents.length]];
-  }
-  else if (type === "industry"){
-      return industryEvents[getRandomNumber[0, industryEvents.length]];
-  }
-  else if (type === "agriculture"){
-      return agricultureEvents[getRandomNumber[0, agricultureEvents.length]];
-  }
-  else if (type === "costumerServices"){
-      return costumerServicesEvents[getRandomNumber[0, costumerServicesEvents.length]];
-  }
-  else if (type === "infrastructure"){
-      return infrastructureEvents[getRandomNumber[0, infrastructureEvents.length]];
-  }
-  else if (type === "tourism"){
-      return tourismEvents[getRandomNumber[0, tourismEvents.length]];
-  }
-  else if (type === "education"){
-      return educationEvents[getRandomNumber[0, educationEvents.length]];
-  }
-  return agricultureEvents[getRandomNumber[0, agricultureEvents.length]];
+//new code
+//is this shorter than your version of code Vuthy?
+function getRandomEvent1(type){//type === the array of the random events
+ return type[Math.floor(Math.random() * type.length)]
 }
